5a08629d7502a85a4cc3d09d20abc810489718d8
NakanoSanku <alexkaykt@outlook.com>
2023-06-17 22:49:17 +0800

    修改小错误

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fp:
     long_description = fp.read()
 
 setup(name='minidevice',
-      version='2.0.2',
+      version='2.0.3',
       description='Android Auto Pypi',
       author='KateTseng',
       author_email='Kate.TsengK@outlook.com',
