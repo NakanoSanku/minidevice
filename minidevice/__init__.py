eb1c5007ef0215e1e5e9118cfde846eff8470f30
KateTseng <Alexkaykt@outlook.com>
2023-07-17 11:18:16 +0800

    update

--- minidevice/__init__.py
+++ minidevice/__init__.py
@@ -3,5 +3,3 @@ from minidevice.adbcap import ADBcap
 from minidevice.adbtouch import ADBtouch
 from minidevice.minicap import Minicap
 from minidevice.minitouch import Minitouch
-from minidevice.scrcpycap import ScrcpyCap
-from minidevice.scrcpytouch import ScrcpyTouch
