a8bfb3a7c300c87e5ae263778c6441491c5bb322
nakanosanku <alexkaykt@outlook.com>
2023-12-02 22:23:49 +0800

    添加多线程实例

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import threading
 from minidevice import MiniDevice
 from minidevice.minicap import Minicap
 from minidevice.minitouch import Minitouch
 a=MiniDevice("emulator-5554",Minicap,Minitouch)
-a.screenshot()
-with open("a.png", "wb") as fp:
-    fp.write(a.screenshot())
-a.click(100,100)
-a.swipe([(10,10),(100,100)])
+def increment():
+    a.screenshot()
+    
+# 创建多个线程来并发访问共享资源
+threads = []
+for _ in range(10):
+    t = threading.Thread(target=increment)
+    threads.append(t)
+    t.start()
+    
+# 等待所有线程完成
+for t in threads:
+    t.join()
+
