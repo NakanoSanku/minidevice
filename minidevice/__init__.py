4719800efa8f2b56243f910f120ff12917ebf877
nakanosanku <alexkaykt@outlook.com>
2023-12-02 21:20:22 +0800

    导入MiniDevice

--- minidevice/__init__.py
+++ minidevice/__init__.py
@@ -5,14 +5,4 @@ from minidevice.minicap import Minicap
 from minidevice.minitouch import Minitouch
 from minidevice.screencap import ScreenCap
 from minidevice.touch import Touch
-
-TOUCH_METHOD = {
-    "Minitouch":Minitouch,
-    "ADBtouch":ADBtouch
-}
-
-CAP_METHOD = {
-    "ADBcap": ADBcap,
-    "Minicap": Minicap,
-    "DroidCast": DroidCast
-}
+from minidevice.device import MiniDevice
