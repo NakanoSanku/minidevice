e87d98424174bc4cb189077d40f755932aebc22e
nakanosanku <alexkaykt@outlook.com>
2023-12-06 10:29:16 +0800

    增强可拓展性，更改screenshot命名为screenshot_raw以保证不会混淆

--- minidevice/device.py
+++ minidevice/device.py
@@ -1,35 +1,53 @@
 from minidevice import *
 import threading
-
+from typing import Union
 from minidevice.logger import logger
 
 
 class MiniDevice:
-    def __init__(self, serial: str, capMethod: ADBtouch | Minicap | DroidCast = None,
-                 touchMethod: ADBtouch | Minitouch = None, screenshotTimeout=500) -> None:
+    def __init__(self, serial=None, capMethod: Union[ADBtouch, Minicap, DroidCast] = None,
+                 touchMethod: Union[ADBtouch, Minitouch] = None, screenshotTimeout=500) -> None:
         """设备操作类
 
         Args:
-            serial (str): 设备id
-            capMethod (CAP_METHOD,optional): ADBtouch | Minicap | DroidCast. Defaults to None
+            serial (str): 设备id 或者 其他初始化设备标识比如句柄
+            capMethod (CAP_METHOD,optional): ADBtouch | Minicap | DroidCast. Defaults to None 
             touchMethod (TOUCH_METHOD, optional): ADBtouch | Minitouch. Defaults to None.
             screenshotTimeout (int, optional):截图延迟，每张截图之间的延迟时间(包含截图过程) 单位ms. Defaults to 500.
         """
         self.__serial = serial
+        self.__capMethod = None
+        self.__touchMethod = None
 
         # TypeError: issubclass() arg 1 must be a class
         def typeAndClassCheck(method, superClass):
-            if method:
-                if isinstance(method, type):
-                    if not issubclass(method, superClass):
-                        raise TypeError(f"{method.__name__} is not a subclass of {superClass.__name__}")
-                else:
-                    raise TypeError(f"{capMethod.__name__} is not a class")
+            if isinstance(method, type):
+                if not issubclass(method, superClass):
+                    raise TypeError(f"{method.__name__} is not a subclass of {superClass.__name__}")
+            else:
+                raise TypeError(f"{method.__name__} is not a class")
 
-        typeAndClassCheck(capMethod, ScreenCap)
-        typeAndClassCheck(touchMethod, Touch)
-        self.__capMethod = capMethod(serial) if capMethod else None
-        self.__touchMethod = touchMethod(serial) if touchMethod else None
+        if capMethod:
+            if isinstance(capMethod, Touch):
+                self.__capMethod = capMethod
+            else:
+                typeAndClassCheck(capMethod,ScreenCap)
+                if self.__serial:
+                    self.__capMethod = capMethod(serial)
+                else:
+                    raise TypeError(f"{capMethod.__name__} missing instantiation parameter")
+
+        if touchMethod:
+            if isinstance(touchMethod, Touch):
+                self.__touchMethod = touchMethod
+            else:
+                typeAndClassCheck(touchMethod, Touch)
+                if self.__serial:
+                    self.__touchMethod = touchMethod(serial)
+                else:
+                    raise TypeError(f"{touchMethod.__name__} missing instantiation parameter")
+
+
         self.__touchThreadLock = threading.Lock()  # 操作线程锁
         self.__touchTimeoutTimer = None  # 操作延迟定时器
         self.__screenshotThreadLock = threading.Lock()  # 截图线程锁
@@ -41,7 +59,7 @@ class MiniDevice:
         """仅用于延迟定时器"""
         pass
 
-    def screenshot(self) -> bytes:
+    def screenshot_raw(self) -> bytes:
         """截图"""
         if self.__capMethod:
             if (
@@ -108,7 +126,6 @@ class MiniDevice:
                 self.__touchMethod.swipe(points, duration)
                 logger.debug(f"swiped {self.__serial} from {points[0]} to {points[-1]}")
 
-
             self.__touchThreadLock.acquire()
             try:
                 if (
@@ -122,10 +139,3 @@ class MiniDevice:
                     threading.Thread(target=waitFunc).start()  # 启动一个新线程在上一个操作执行完后执行当前操作
             finally:
                 self.__touchThreadLock.release()
-
-    def __del__(self):
-        """手动回收截图和操作对象"""
-        if self.__touchMethod:
-            del self.__touchMethod
-        if self.__capMethod:
-            del self.__capMethod
