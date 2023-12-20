f3318c7667933a0fe5eb4d0dc67f235a6ccbe1ba
nakanosanku <alexkaykt@outlook.com>
2023-12-20 12:44:49 +0800

    添加adb截图异常处理

--- minidevice/adbcap.py
+++ minidevice/adbcap.py
@@ -14,11 +14,26 @@ class ADBcap(ScreenCap):
         self.__adb = adb.device(serial)
 
     def screencap_raw(self) -> bytes:
-        adb_command = [adb_path(), "-s", self.__adb.serial, "exec-out", "screencap", "-p"]
-        process = subprocess.Popen(
-            adb_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        data, err = process.communicate(timeout=10)
-        
-        if process.returncode == 0:
-            return data
+        """
+        截图并以字节流的形式返回Android设备的屏幕。
+
+        :param serial: 设备的序列号。
+        :return: 截图的字节数据。
+        """
+        try:
+            adb_command = [adb_path(), "-s", self.__adb.serial, "exec-out", "screencap", "-p"]
+            process = subprocess.Popen(
+                adb_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            data, err = process.communicate(timeout=10)
+            
+            if process.returncode == 0 and data:
+                return data
+            else:
+                raise subprocess.TimeoutExpired(
+                    None, timeout=10, stdout=data, stderr=err
+                )
+        except subprocess.TimeoutExpired as e:
+            raise e
+        except Exception as e:
+            raise RuntimeError(f"Error while screencapping the device: {e}")
